b6cfa2b615d4d6d2ef2a0245c04ed28dd80f25e5
E1-80750-krushna <krushnamhaske962@gmail.com>
2024-01-01 14:51:17 +0000

    this demo

---
+++ demo1.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "enter the m"
+read m
+case $m in 
+	1)echo "feb is 28 days"
+	;;
+	2)echo "march is 31 days"
+	;;
+	3)echo "april is 30 days"
+	
+	
+	tfujgulhlh;jj
+	;;
+	*)echo "invalid month"
+
+esac
+
+
